8343713de571784211b5678d6e6e32ebb5b38d1c
JoaoCCTeixeira <joaoccaleixo@gmail.com>
2024-11-07 21:25:50 +0000

    Criação da class "Reserva"

---
+++ Models/Reserva.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Reserva
+    {
+        public int IdReserva { get; set; }
+
+        [Required]
+        public string NomeCliente { get; set; }
+
+        [Range(1, 35)]
+        public int NumeroMesa { get; set; }
+
+        [Required(ErrorMessage = "O número da mesa é obrigatório.")]
+        [Range(1, 20)]
+        public int NumeroPessoas { get; set; }
+
+        [Required]
+        public DateTime DataHora { get; set; }
+
+        public string Observacao { get; set; }
+    }
+}
