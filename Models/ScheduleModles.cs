52daae720dad6965a4f6ac5d25a6f532c6babbeb
Gustavo Arroja <arrojag@gmail.com>
2024-11-08 17:10:34 +0000

    creation of the model Schedule to be the base of reference for the view and controller, and to add the Group5's description

---
+++ Models/ScheduleModles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReserveSystem.Models
+{
+    public class Schedule
+    {
+        [Key]
+        [Required]
+        public int ScheduleId { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public DateTime StartShiftTime { get; set; }
+
+        [Required]
+        public DateTime EndShiftTime { get; set; }
+
+        // Chave estrangeira
+        [Required]
+        public int StaffId { get; set; }
+
+        // Propriedade de navegação
+        [ForeignKey("StaffId")]
+        public String Staff { get; set; }
+
+        public bool IsValidShiftTime()
+        {
+            return EndShiftTime > StartShiftTime;
+        }
+    }
+}
