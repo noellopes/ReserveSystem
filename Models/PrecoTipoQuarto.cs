9d48858afab482fc51af0e24827798a2bd465533
AnaT1999 <anabeatriztavares2004@gmail.com>
2024-11-08 12:31:09 +0000

    criação de variáveis

---
+++ Models/PrecoTipoQuarto.cs
@@ -0,0 +1,12 @@
+﻿using ReserveSystem.Models;
+
+public class PrecoTipoQuarto
+{
+	public PrecoTipoQuarto()
+	{
+		public int precotq_id {  get; set; }
+
+		[Required]
+		public	float PrecoBase {  get; set; }
+	}
+}
