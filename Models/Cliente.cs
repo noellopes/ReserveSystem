f236f6349d4190bfd5343d9da21169d02e1f5813
Gonçalo Figueiredo <1707970@sal.ipg.pt>
2024-11-08 12:04:09 +0000

    Adicionar Model Clientes

---
+++ Models/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Cliente
+    {
+        [Key][Required]
+        public int ClienteId { get; set; }
+
+        [Required]
+        public String Nome { get;set; }
+
+        [Required]
+        public String Email { get; set; }
+
+        [Required]
+        public String Telefone { get; set; }
+
+        [Required]
+        public bool Login { get; set; }
+
+        [Required]
+        public String Nif  { get; set; }
+
+    }
+}
