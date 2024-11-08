85fb30483283018ce32cdceb7b2479914177fe92
Ricardo Sousa <ricardoavelal@gmail.com>
2024-11-08 13:24:03 +0000

    -Adicionado Modelo da Ementa e Pratos

---
+++ Models/Ementa.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Ementa
+    {
+        
+        public int EmentaId { get; set; }
+
+        [Required]
+
+        public int IdPratos { get;}
+
+        [Required]
+        public int IdSobremesas { get; }
+
+        [Required]
+
+        public int IdSopas { get; }
+
+        [Required]
+
+        public int IdBebidas { get; }
+
+        [Required]
+
+    }
+}
---
+++ Models/Pratos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Pratos
+    {
+        public int IdPrato {  get; set; }
+        [Required]
+
+        public string PratoNome { get; set; }
+        [Required]
+        public string Descrição { get; set; }
+        [Required]
+
+
+    }
+}
+
