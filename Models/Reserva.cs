46a6ed7ad5d960d4d07f5feec1acfb686b6817a5
RafaelTeixeira24 <rafael.teix.24@gmail.com>
2024-11-08 12:04:04 +0000

    Adicionar Model Reserva

---
+++ Models/Reserva.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Reserva
+    {
+
+        [Key] [Required] 
+        int ReservaId { get; set; }
+
+        [Required]
+        int ClienteId { get; set; }
+
+        [Required]
+        DateTime DataCheckIn { get; set; }
+
+        [Required]
+        DateTime DataCheckOut { get; set; }
+
+        [Required]
+        DateTime DataReserva { get; set; }
+
+        [Required]
+        bool EstadoPagamento { get; set; }
+
+        [Required]
+        string Estado { get; set; }
+
+        [Required]
+        int NumeroPessoas { get; set; }
+
+
+    }
+}
