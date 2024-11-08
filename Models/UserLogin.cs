dc6213412446eae82a9d474baeb5990b07308bc2
Jorge <845067@unizar.es>
2024-11-08 12:59:41 +0000

    login class

---
+++ Models/UserLogin.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class UserLogin
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int IdPrivilegio { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
