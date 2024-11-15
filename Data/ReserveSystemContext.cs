9b2b7d46c5129bf8de1860f249c7465bf37b6c6d
pfjndev <88943477+pfjndev@users.noreply.github.com>
2024-11-15 13:20:03 +0000

    Add data migrations.

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ReserveSystem.Models;
 
 namespace ReserveSystem.Data
 {
     public class ReserveSystemContext : DbContext
     {
         public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) {}
+        public DbSet<ReserveSystem.Models.RoomServiceBooking> RoomServiceBooking { get; set; } = default!;
     }
 }
