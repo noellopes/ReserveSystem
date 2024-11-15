658d6451e2fd5c93da7edc10c7cb5297377cf9d4
Pedro-Costa-PC <pedrodcosta@deloitte.com>
2024-11-15 13:26:39 +0000

    Creating the CRUD of the cliente

---
+++ Models/ClienteTestModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class ClienteTestModel
+    {
+
+        [Key] 
+        public int ClienteId { get; set; }
+
+        [Required] 
+        [StringLength(100)] 
+        public string Nome { get; set; }
+
+        [Required]
+        [EmailAddress] 
+        public string Email { get; set; }
+
+        [Phone]
+        public string Telefone { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayName("Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+    }
+}
+
