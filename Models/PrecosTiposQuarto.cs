bdd025acc66c3c502af1eaba23abb04fe66d0209
AnaT1999 <anabeatriztavares2004@gmail.com>
2024-11-15 11:59:18 +0000

    update dos models e das variaveis

---
+++ Models/PrecosTiposQuarto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models;
+public class PrecoTipoQuarto : Reserva
+{
+    [Required]
+    public int precotq_id { get; set; }
+    [Required]
+    public string tipo { get; set; }
+    [Required]
+    public int quantidadeQuartos { get; set; }
+    [Required]
+    public int capacidade { get; set; }
+
+    [Required]
+		public	float PrecoBase {  get; set; }
+
+    [Required]
+    public float taxaCancelamento { get; set; }
+
+    [Required]
+    public float taxaAdiconal { get; set; }
+
+}
+
---
+++ Models/Reserva.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+
+namespace ReserveSystem.Models;
+public class Reserva
+{
+	
+        [Required]
+        public int numeroReserva {  get; set; }
+
+        [Required]
+        public  DateTime dataCheckIn { get; set; }
+
+        [Required]
+        public DateTime dataCheckOut { get; set; }
+
+        [Required]
+        public DateTime dataReserva { get; set; }
+
+        [Required]
+        public DateTime dataCancelamento { get; set; }
+
+       
+        public bool cancelamento { get; set; }
+
+        [Required]
+        public string Estado {  get; set; }
+
+        [Required]
+        public bool EstadoPagamento { get; set; }
+
+        [Required]
+        public int NumeroPessoas { get; set; }
+
+
+
+}
