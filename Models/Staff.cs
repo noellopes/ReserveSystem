feffd2098b27fea895981df1e0fe6d6f0a34f6d5
krmk <keremkaraca037@gmail.com>
2024-11-15 16:29:16 +0300

    Staff Class added

---
+++ Models/Staff.cs
@@ -0,0 +1,17 @@
+﻿namespace ReserveSystem.Models
+
+{
+    public class Staff
+    {
+        [Key]
+        [Required]
+        public int StaffID { get; set; }
+        public string StaffName { get; set; }
+        public string StaffEmail { get; set; }
+        public string StaffDept { get; set; }
+        public string StartFunctionsDate { get; set; }
+        public string EndFunctionsDate { get; set; }
+        public int JobId { get; set; }
+
+    }
+}
