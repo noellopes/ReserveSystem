f20d659e23adee54df26c58a55c27b78e291b7fe
anilhazar <alianilhazar@gmail.com>
2024-11-17 22:58:46 +0000

    Add the configurations to DB Context for Sala and TipoSala Entites

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -8,5 +8,10 @@ namespace ReserveSystem.Data
         public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) {}
         public DbSet<ReserveSystem.Models.ClienteModel> ClienteModel { get; set; } = default!;
         public DbSet<ReserveSystem.Models.ReservaModel> ReservaModel { get; set; } = default!;
+        
+        public DbSet<Sala> Salas { get; set; } = default!;
+        
+        public DbSet<TipoSala> TipoSalas { get; set; } = default!;
+        
     }
 }
