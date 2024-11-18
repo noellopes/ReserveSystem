022d08d5ac572c503e9cc30bfa4848b4ae16c14d
Rodrigo Paula <rnpaula2000@gmail.com>
2024-11-18 20:56:37 +0000

    tabela job funcional

--- Controllers/JobModelsController.cs
+++ Controllers/JobModelsController.cs
@@ -54,7 +54,7 @@ namespace ReserveSystem.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("jobID,jobDescription")] JobModel jobModel)
+        public async Task<IActionResult> Create([Bind("jobID,jobName,jobDescription")] JobModel jobModel)
         {
             if (ModelState.IsValid)
             {
@@ -86,7 +86,7 @@ namespace ReserveSystem.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("jobID,jobDescription")] JobModel jobModel)
+        public async Task<IActionResult> Edit(int id, [Bind("jobID,jobName,jobDescription")] JobModel jobModel)
         {
             if (id != jobModel.jobID)
             {
