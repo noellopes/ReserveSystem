f49d7b3199cc3e578713d1c089b6e936901224d0
RafaelTeixeira24 <rafael.teix.24@gmail.com>
2024-11-18 22:12:16 +0000

    FinalUpdate ReservaModel

---
+++ Models/ReservaModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReserveSystem.Models
+{
+    public class ReservaModel
+    {
+
+        [Key]
+        public int ID_BOOKING { get; set; }
+        [Required(ErrorMessage = "Data de CheckIn")]
+        public DateTime CHECKIN_DATE { get; set; }
+        [Required(ErrorMessage = "Data de CheckOut")]
+        public DateTime CHECKOUT_DATE { get; set; }
+        [Required(ErrorMessage = "Data de Reserva")]
+        public DateTime BOOKING_DATE { get; set; }
+        [Required(ErrorMessage = "Estado de Reserva é obrigatório")]
+        public string BOOKED { get; set; }
+        public bool PAYMENT_STATUS { get; set; }
+        [Required(ErrorMessage = "Número da pessoa")]
+        public int TOTAL_PERSONS_NUMBER { get; set; }
+
+        [ForeignKey("ID_CLIENT")]
+        public ClienteModel? Cliente { get; set; }
+    }
+}
