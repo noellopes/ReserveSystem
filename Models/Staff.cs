e85fd2fea577205285bb2385da97bd8f5ee3462f
krmk <keremkaraca037@gmail.com>
2024-11-18 20:09:12 +0300

    foreign key added and required tags

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -7,13 +7,25 @@
         [Required]
         public int StaffID { get; set; }
         [Required]
+        [ForeignKey("Job")]
+        public int JobId { get; set; }
+
+        [Required]
+        [StringLenght(100)]
         public string StaffName { get; set; }
         [Required]
         public string StaffEmail { get; set; }
+
+        [StringLenght(50)]
         public string StaffDept { get; set; }
+        public string StaffPhone { get; set; }
+        [Required]
+        [MinLenght(8)]
+        public string StaffPassword { get; set; }
+
+        [Required]
         public string StartFunctionsDate { get; set; }
         public string EndFunctionsDate { get; set; }
-        public int JobId { get; set; }
         public int DaysOffVacation { get; set; }
 
     }
