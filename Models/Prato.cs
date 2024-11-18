94cac9d5bc05b95b29c93f2349cb625798dcbfe6
tomasreigota <166636231+tomasr79@users.noreply.github.com>
2024-11-18 23:19:36 +0000

    Revert "Revert "Create PratoModel, Controller and the views, add the PratoTable on da…""

---
+++ Models/Prato.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Prato
+    {
+        public int PratoID { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O nome do prato é obrigatório"), StringLength(300)]
+        public string Nome { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O descrição é obrigatória"), StringLength(300)]
+        public string Descricao { get; set; }
+    }
+}
