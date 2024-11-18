c4eb93454cf2539f9b53fca4d914c101c8c6d996
Gonçalo Figueiredo <1707970@sal.ipg.pt>
2024-11-18 18:16:51 +0000

    .

---
+++ Controllers/RoomsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ReserveSystem.Data;
+using ReserveSystem.Models;
+
+namespace ReserveSystem.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly ReserveSystemContext _context;
+
+        public RoomsController(ReserveSystemContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rooms
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Rooms.ToListAsync());
+        }
+
+        // GET: Rooms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rooms = await _context.Rooms
+                .FirstOrDefaultAsync(m => m.RoomTypeId == id);
+            if (rooms == null)
+            {
+                return NotFound();
+            }
+
+            return View(rooms);
+        }
+
+        // GET: Rooms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RoomTypeId,RoomType,Capacity,NumberOfRooms,HasView,AdaptedRoom")] Rooms rooms)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(rooms);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rooms);
+        }
+
+        // GET: Rooms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rooms = await _context.Rooms.FindAsync(id);
+            if (rooms == null)
+            {
+                return NotFound();
+            }
+            return View(rooms);
+        }
+
+        // POST: Rooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RoomTypeId,RoomType,Capacity,NumberOfRooms,HasView,AdaptedRoom")] Rooms rooms)
+        {
+            if (id != rooms.RoomTypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rooms);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomsExists(rooms.RoomTypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(rooms);
+        }
+
+        // GET: Rooms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var rooms = await _context.Rooms
+                .FirstOrDefaultAsync(m => m.RoomTypeId == id);
+            if (rooms == null)
+            {
+                return NotFound();
+            }
+
+            return View(rooms);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var rooms = await _context.Rooms.FindAsync(id);
+            if (rooms != null)
+            {
+                _context.Rooms.Remove(rooms);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomsExists(int id)
+        {
+            return _context.Rooms.Any(e => e.RoomTypeId == id);
+        }
+    }
+}
---
+++ Models/Rooms.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Rooms
+    {
+        [Key][Required] public int RoomTypeId {get; set;}
+
+        [Required] public int RoomType {get; set;}
+
+        [Required] public int Capacity {get; set;}
+
+        [Required] public int NumberOfRooms { get;set;}
+
+        [Required] public bool HasView {get; set;}
+
+        [Required] public string AdaptedRoom { get; set; }
+
+
+    }
+}
