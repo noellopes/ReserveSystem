542921ea1cc2ffb7e83c2f0da51d022355fa5790
CutThroat2908 <rnpaula2000@gmail.com>
2024-11-18 17:12:00 +0000

    testes

---
+++ Data/Migrations/20241118171024_JobModels.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReserveSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class JobModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_JobModel_JobModel_JobModeljobID",
+                table: "JobModel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_JobModel_JobModeljobID",
+                table: "JobModel");
+
+            migrationBuilder.DropColumn(
+                name: "JobModeljobID",
+                table: "JobModel");
+
+            migrationBuilder.AddColumn<string>(
+                name: "jobName",
+                table: "JobModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "jobName",
+                table: "JobModel");
+
+            migrationBuilder.AddColumn<int>(
+                name: "JobModeljobID",
+                table: "JobModel",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobModel_JobModeljobID",
+                table: "JobModel",
+                column: "JobModeljobID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_JobModel_JobModel_JobModeljobID",
+                table: "JobModel",
+                column: "JobModeljobID",
+                principalTable: "JobModel",
+                principalColumn: "jobID");
+        }
+    }
+}
