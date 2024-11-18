4f7d27004e7549049b46ff1db49f981c27be4c40
Gonçalo Figueiredo <1707970@sal.ipg.pt>
2024-11-18 18:16:31 +0000

    .

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -49,6 +49,11 @@ namespace ReserveSystem.Controllers
             return View();
         }
 
+        public IActionResult Details()
+        {
+            return View();
+        }
+
         // POST: Clientes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
