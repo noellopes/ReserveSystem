0e82bf77d202bdfcad4d82519a6420be66818e51
Mine Safak <haticemine352@gmail.com>
2024-11-18 17:00:46 +0000

    Delete unnecessary file

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -6,6 +6,6 @@ namespace ReserveSystem.Data
     public class ReserveSystemContext : DbContext
     {
         public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) {}
-        public DbSet<ReserveSystem.Models.Ingrediente> Ingrediente { get; set; } = default!;
+     
     }
 }
