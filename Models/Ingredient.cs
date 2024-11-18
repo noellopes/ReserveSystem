b9353643434627a1bfc61ab6a697c082e8a4bd2a
filipecampos9 <fjcampos.fc@gmail.com>
2024-11-18 16:23:15 +0000

    Create Ingredient Model, Controller and pages to create, edit, delete, etc.. using scaffolding

---
+++ Models/Ingredient.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Ingredient
+    {
+        
+        public int IngredientID { get; set; }
+
+        [Display(Name = "Ingredient Name")]
+        [Required(ErrorMessage = "Ingredient Name is required"), StringLength(300)]
+        public string Name { get; set; }
+
+        [Display(Name = "Unity Of Measure")]
+        [Required(ErrorMessage = "Unity of measure is required"), StringLength(10)]
+        public double UnityMeasure { get; set; }
+
+    }
+}
