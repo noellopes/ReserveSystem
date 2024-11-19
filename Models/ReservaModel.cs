8dc40911565ef717139695832bc354d5571960f6
Edson Muandula <emuandula69@gmail.com>
2024-11-19 12:36:31 +0000

    - Delete models that has nothing to do with our group - Delete the reletionship between Reserva and Equipamento

--- Models/ReservaModel.cs
+++ Models/ReservaModel.cs
@@ -8,18 +8,15 @@ namespace ReserveSystem.Models
     {
         [Key]
         public int ReservaID { get; set; }
-   
-        public string TipoReserva { get; set; }// fica como um dropdown//select tag 
+
+        public string TipoReserva { get; set; }// fica como um dropdown - select tag 
         public DateTime DataReserva { get; set; }
         public DateTime DataInicio { get; set; }
         public DateTime DataFim { get; set; }
         public int Partcipantes { get; set; }
         public double PrecoTotal { get; set; }
         public int ClienteId { get; set; }
-        public ClienteModel? Cliente { get; set; }
-        [ForeignKey("Equipamento")]
-        public int IdEquipamento { get; set; }  // Foreign key to Equipamento
-        public Equipamento Equipamento { get; set; }  // Navigation property
+        public ClienteModel? Cliente { get; set; } 
 
     }
 }
