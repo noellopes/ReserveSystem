eb422e292a0baf1dab54ea8d2d32faaa6f180a10
IDontHaveAUserN <ruthokoduwa123@gmail.com>
2024-11-19 05:47:10 +0000

    Added a NIF validator, a select, some css and js for country's code and flag

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+    document.getElementById("countrySelect").addEventListener("change", function() {
+        const selectedOption = this.options[this.selectedIndex];
+    const countryCode = selectedOption.getAttribute("data-code");
+        const countryFlagCode = selectedOption.getAttribute("data-flag");
+        const phoneLength = selectedOption.getAttribute("data-length");
+
+    // Display the country code in the telephone input field
+    const countryCodeElement = document.getElementById("telefone");
+        countryCodeElement.value = countryCode + " ";
+        countryCodeElement.setAttribute("maxlength", phoneLength);
+
+    // Fetch the flag image from the API
+        const flagUrl = `https://flagcdn.com/w320/${countryFlagCode}.png`;        
+
+    // Display the flag image
+    const flagElement = document.getElementById("flag");
+    flagElement.src = flagUrl;
+        document.getElementById("flagIcon").style.display = "inline"; // Show the flag icon        
+    });
+
+    // Trigger the change event on page load to set the default flag (optional)
+    document.getElementById("countrySelect").dispatchEvent(new Event("change"));
+
+
