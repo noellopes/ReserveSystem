3b361b782d2db264e53c8d08d750f60fb273bc94
AnaT1999 <anabeatriztavares2004@gmail.com>
2024-11-19 14:53:44 +0000

    commit 3 primeira migração e criação de tabela/base de dados para o model TipoQuarto

---
+++ Data/Migrations/20241119144817_TabelaTipoQuarto.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReserveSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TabelaTipoQuarto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoQuarto",
+                columns: table => new
+                {
+                    TipoQuartoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    type = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    capacity = table.Column<int>(type: "int", nullable: false),
+                    RoomQuantity = table.Column<int>(type: "int", nullable: false),
+                    AcessibilityRoom = table.Column<bool>(type: "bit", nullable: false),
+                    View = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoQuarto", x => x.TipoQuartoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TipoQuarto");
+        }
+    }
+}
