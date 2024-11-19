bbbde4f807d5f11c79e4513566d9d1f8e8f4e29b
AnaT1999 <anabeatriztavares2004@gmail.com>
2024-11-19 17:54:00 +0000

    Commit 5 = Criação com sucesso das migrações e da tabela/database da classe PrecoTipoQuarto

--- Data/Migrations/20241119175149_TabelaPrecoTipoQuarto.Designer.cs
+++ Data/Migrations/20241119175149_TabelaPrecoTipoQuarto.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReserveSystem.Data;
 
@@ -11,9 +12,11 @@ using ReserveSystem.Data;
 namespace ReserveSystem.Data.Migrations
 {
     [DbContext(typeof(ReserveSystemUsersDbContext))]
-    partial class ReserveSystemUsersDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241119175149_TabelaPrecoTipoQuarto")]
+    partial class TabelaPrecoTipoQuarto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20241119175149_TabelaPrecoTipoQuarto.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReserveSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class TabelaPrecoTipoQuarto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PrecoTipoQuarto",
+                columns: table => new
+                {
+                    id_RTPrice = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BasePrice = table.Column<float>(type: "real", nullable: false),
+                    CancelationFee = table.Column<float>(type: "real", nullable: false),
+                    AdicionalBeds = table.Column<float>(type: "real", nullable: false),
+                    TipoQuartoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PrecoTipoQuarto", x => x.id_RTPrice);
+                    table.ForeignKey(
+                        name: "FK_PrecoTipoQuarto_TipoQuarto_TipoQuartoId",
+                        column: x => x.TipoQuartoId,
+                        principalTable: "TipoQuarto",
+                        principalColumn: "TipoQuartoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PrecoTipoQuarto_TipoQuartoId",
+                table: "PrecoTipoQuarto",
+                column: "TipoQuartoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PrecoTipoQuarto");
+        }
+    }
+}
