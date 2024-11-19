7a38f43700490eeabd38c2dfefbcedcb482dc9f2
Edson Muandula <emuandula69@gmail.com>
2024-11-19 12:35:17 +0000

    Add new equipments to the list

--- Controllers/EquipamentoController.cs
+++ Controllers/EquipamentoController.cs
@@ -16,17 +16,40 @@ namespace ReserveSystem.Controllers
 
         public IActionResult Index()
         {
+            if (_context.Equipamento.Any())
+            {
+                _context.Equipamento.RemoveRange(_context.Equipamento);
+                _context.SaveChanges();
+            }
+
             if (!_context.Equipamento.Any())
             {
+
                 var listaEquipamentos = new List<Equipamento>
                 {
-                    new Equipamento { NomeEquipamento = "Projetor", TipoEquipamento = "Eletrônico", Quantidade = 3 },
-                    new Equipamento { NomeEquipamento = "Sistema de Som", TipoEquipamento = "Eletrônico", Quantidade = 2 },
-                    new Equipamento { NomeEquipamento = "Microfone", TipoEquipamento = "Eletrônico", Quantidade = 5 },
-                    new Equipamento { NomeEquipamento = "Quadro Branco", TipoEquipamento = "Mobiliário", Quantidade = 3 },
-                    new Equipamento { NomeEquipamento = "Mesa de Conferência", TipoEquipamento = "Mobiliário", Quantidade = 1 }
+                    new Equipamento { NomeEquipamento = "Projector", TipoEquipamento = "Electronic", Quantidade = 3 },
+                    new Equipamento { NomeEquipamento = "Sound System", TipoEquipamento = "Electronic", Quantidade = 8 },
+                    new Equipamento { NomeEquipamento = "Microphone", TipoEquipamento = "Electronic", Quantidade = 30 },
+                    new Equipamento { NomeEquipamento = "Whiteboard", TipoEquipamento = "Furniture", Quantidade = 7 },
+                    new Equipamento { NomeEquipamento = "Conference Table", TipoEquipamento = "Furniture", Quantidade = 10 },
+                    new Equipamento { NomeEquipamento = "Chairs", TipoEquipamento = "Furniture", Quantidade = 500 },
+                    new Equipamento { NomeEquipamento = "Podium", TipoEquipamento = "Furniture", Quantidade = 5 },
+                    new Equipamento { NomeEquipamento = "Video Conferencing System", TipoEquipamento = "Electronic", Quantidade = 2 },
+                    new Equipamento { NomeEquipamento = "Stage Lighting", TipoEquipamento = "Lighting", Quantidade = 10 },
+                    new Equipamento { NomeEquipamento = "Sound Mixer", TipoEquipamento = "Electronic", Quantidade = 1 },
+                    new Equipamento { NomeEquipamento = "Projection Screen", TipoEquipamento = "Furniture", Quantidade = 5 },
+                    new Equipamento { NomeEquipamento = "Flipcharts", TipoEquipamento = "Furniture", Quantidade = 4 },
+                    new Equipamento { NomeEquipamento = "Wi-Fi Router", TipoEquipamento = "Electronic", Quantidade = 5 },
+                    new Equipamento { NomeEquipamento = "Laptop", TipoEquipamento = "Electronic", Quantidade = 10 },
+                    new Equipamento { NomeEquipamento = "Power Strips", TipoEquipamento = "Accessories", Quantidade = 10 },
+                    new Equipamento { NomeEquipamento = "HDMI Cables", TipoEquipamento = "Accessories", Quantidade = 13 },
+                    new Equipamento { NomeEquipamento = "Portable Speaker", TipoEquipamento = "Electronic", Quantidade = 3 },
+                    new Equipamento { NomeEquipamento = "Laser Pointer", TipoEquipamento = "Accessories", Quantidade = 5 },
+                    new Equipamento { NomeEquipamento = "Coffee Maker", TipoEquipamento = "Kitchen Equipment", Quantidade = 11 },
+                    new Equipamento { NomeEquipamento = "Thermos", TipoEquipamento = "Kitchen Equipment", Quantidade = 5 }
                 };
 
+              
                 _context.Equipamento.AddRange(listaEquipamentos);
                 _context.SaveChanges();
             }
@@ -40,7 +63,7 @@ namespace ReserveSystem.Controllers
             var equipamento = _context.Equipamento.Find(id);
             if (equipamento == null)
             {
-                return NotFound();
+                return NotFound();  
             }
             return View(equipamento);
         }
@@ -94,5 +117,12 @@ namespace ReserveSystem.Controllers
           // You can store the issue report in a database table or send a notification, etc.
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult CheckEquipments()
+        {
+            var equipamentos = _context.Equipamento.ToList();
+            return Json(equipamentos);
+        }
+
     }
 }
