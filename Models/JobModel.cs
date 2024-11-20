f146f2be468cec5af8568d05a9bd6b0895bf3e1a
Rodrigo Paula <rnpaula2000@gmail.com>
2024-11-20 21:37:37 +0000

    Job tweaks

--- Models/JobModel.cs
+++ Models/JobModel.cs
@@ -6,12 +6,15 @@ namespace ReserveSystem.Models
     {
         [Key]
         [Required(ErrorMessage = "ID is required")]
+        [Display(Name = "Job ID")]
         public int jobID { get; set; }
 
         [Required(ErrorMessage = "Job name is required")]
+        [Display(Name = "Job")]
         public required String jobName { get; set; }
 
         [Required(ErrorMessage = "Job description is required")]
+        [Display(Name = "Description")]
         public required String jobDescription { get; set; }
     }
 }
