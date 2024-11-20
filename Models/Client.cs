be2845c135c01578ed7a84f691656645b0351a2c
Rui Ramos <1704711@sal.ipg.pt>
2024-11-20 15:21:28 +0000

    Create Model Client

---
+++ Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace ReserveSystem.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string CleanPreference { get; set; }
+    }
+}
