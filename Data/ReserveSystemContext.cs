245426ee3a5756a3c6d1d18a9d2c09e0306c96a2
Rui Ramos <1704711@sal.ipg.pt>
2024-11-20 15:28:40 +0000

    Scaffolding for Client Model

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ReserveSystem.Models;
 
 namespace ReserveSystem.Data
 {
     public class ReserveSystemContext : DbContext
     {
         public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) {}
+        public DbSet<ReserveSystem.Models.Client> Client { get; set; } = default!;
     }
 }
