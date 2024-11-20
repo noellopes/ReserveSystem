85882c4f67d73b17228da7c4a0af16ed80454e79
Rui Ramos <1704711@sal.ipg.pt>
2024-11-20 20:47:45 +0000

    Create Employee Model

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public required string Name { get; set; }
+        public required string Type { get; set; }
+        public required TimeSpan WorkSchedule { get; set; }
+    }
+}
