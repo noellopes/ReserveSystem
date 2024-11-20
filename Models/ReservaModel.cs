a67b2546aff029f3ce330f53cba639880c7a5519
R0dr1g0P <rodrigojcpinto2004@gmail.com>
2024-11-20 11:50:38 +0000

    Updating "ReservaModel", Booking Views and some HTML & CSS on Index

--- Models/ReservaModel.cs
+++ Models/ReservaModel.cs
@@ -8,18 +8,18 @@ namespace ReserveSystem.Models
 
         [Key]
         public int ID_BOOKING { get; set; }
-        [Required(ErrorMessage = "Data de CheckIn")]
-        public DateTime CHECKIN_DATE { get; set; }
-        [Required(ErrorMessage = "Data de CheckOut")]
-        public DateTime CHECKOUT_DATE { get; set; }
-        [Required(ErrorMessage = "Data de Reserva")]
-        public DateTime BOOKING_DATE { get; set; }
-        [Required(ErrorMessage = "Estado de Reserva é obrigatório")]
-        public bool BOOKED { get; set; }
-        public bool PAYMENT_STATUS { get; set; }
-        [Required(ErrorMessage = "Número da pessoa")]
-        public int TOTAL_PERSONS_NUMBER { get; set; }
 
+        [Required(ErrorMessage = "Inserir Data de Check-In")]
+        [DataType(DataType.Date)]
+        public DateTime CHECKIN_DATE { get; set; }
+
+        [Required(ErrorMessage = "Inserir Data de Check-Out")]
+        [DataType(DataType.Date)]
+        public DateTime CHECKOUT_DATE { get; set; }
+
+        [Required(ErrorMessage = "Inserir numero de pessoas")]
+        public int TOTAL_PERSONS_NUMBER { get; set; }
+        
         [ForeignKey("ID_CLIENT")]
         public ClienteModel? Cliente { get; set; }
     }
