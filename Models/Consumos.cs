e5639ece2df3cd46145d4798976a852a94058e5b
RafaelBatista8901 <rafaelbatista8901@gmail.com>
2024-11-21 22:08:26 +0000

    Create the Consumos model

---
+++ Models/Consumos.cs
@@ -0,0 +1,13 @@
+﻿namespace ReserveSystem.Models
+{
+    public class Consumos
+    {
+        public int ConsumosId { get; set; }
+
+        public ICollection<ItemQuarto> ItemQuartos { get; set; } = new List<ItemQuarto>();
+
+        public int QuantidadeConsumida { get; set; }
+
+        public DateTime DataConsumo { get; set; }
+    }
+}
