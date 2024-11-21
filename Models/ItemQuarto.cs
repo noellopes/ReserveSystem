e495564612f6292b8a39df068960ec8741029e81
João Costa <1703762@sal.ipg.pt>
2024-11-21 21:32:12 +0000

    Criação dos modelos ItemQuarto e Quarto

---
+++ Models/ItemQuarto.cs
@@ -0,0 +1,12 @@
+﻿namespace ReserveSystem.Models
+{
+    public class ItemQuarto
+    {
+        public int ItemQuartoId { get; set; }
+
+        public int QuantidadeQuarto { get; set; }
+
+        //public ICollection<Items> Items { get; set; } = new List<Items>();
+
+    }
+}
