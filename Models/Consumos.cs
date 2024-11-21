cfe8c2564f2dd7ccb50520f6cf79dad90e54b3b8
RafaelBatista8901 <rafaelbatista8901@gmail.com>
2024-11-21 22:16:20 +0000

    Validations of the Items and Consumos

--- Models/Consumos.cs
+++ Models/Consumos.cs
@@ -1,4 +1,6 @@
-﻿namespace ReserveSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
 {
     public class Consumos
     {
@@ -6,8 +8,11 @@
 
         public ICollection<ItemQuarto> ItemQuartos { get; set; } = new List<ItemQuarto>();
 
+        [Required(ErrorMessage = "Quantidade is required.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Quantidade consumida tem de ser maior ou igual que 0.")]
         public int QuantidadeConsumida { get; set; }
 
+        [Required(ErrorMessage = "DataConsumo is required.")]
         public DateTime DataConsumo { get; set; }
     }
 }
--- Models/Items.cs
+++ Models/Items.cs
@@ -7,8 +7,11 @@ namespace ReserveSystem.Models
         [Key]
         public int ItemId { get; set; }
 
+        [Required(ErrorMessage = "Name is required.")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Quantidade is required.")]
+        [Range(0, double.MaxValue, ErrorMessage = "Quantidade em armazem tem de ser maior ou igual que 0.")]
         public int QuantidadeArmazem { get; set; }
     }
 }
