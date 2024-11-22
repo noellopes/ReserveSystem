aa361ccac46d9ea73d06f3797284537ac2c70327
krmk <keremkaraca037@gmail.com>
2024-11-22 03:47:15 +0300

    Staff Reserve System Context

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -5,5 +5,7 @@ namespace ReserveSystem.Data
     public class ReserveSystemContext : DbContext
     {
         public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) {}
+        public DbSet<ReserveSystem.Models.Staff> Staff { get; set; } = default!;
+
     }
 }
