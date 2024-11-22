90171f17eda1de3701553878ba8abc57abedfbba
krmk <keremkaraca037@gmail.com>
2024-11-22 04:41:24 +0300

    Staff Controller added

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReserveSystem.Models;
+using System.Threading.Tasks;
+
+namespace ReserveSystem.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StaffController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Staff
+        public async Task<IActionResult> Index()
+        {
+            var staffList = await _context.Staff.Include(s => s.Job).ToListAsync();
+            return View(staffList);
+        }
+
+        // GET: Staff/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staff
+                .Include(s => s.Job)
+                .FirstOrDefaultAsync(m => m.StaffID == id);
+
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return View(staff);
+        }
+
+        // GET: Staff/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Staff/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StaffID,JobId,StaffName,StaffEmail,StaffDept,StaffPhone,StaffPassword,StartFunctionsDate,EndFunctionsDate,DaysOffVacation")] Staff staff)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(staff);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(staff);
+        }
+
+        // GET: Staff/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staff.FindAsync(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return View(staff);
+        }
+
+        // POST: Staff/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("StaffID,JobId,StaffName,StaffEmail,StaffDept,StaffPhone,StaffPassword,StartFunctionsDate,EndFunctionsDate,DaysOffVacation")] Staff staff)
+        {
+            if (id != staff.StaffID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(staff);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StaffExists(staff.StaffID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(staff);
+        }
+
+        // GET: Staff/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var staff = await _context.Staff
+                .Include(s => s.Job)
+                .FirstOrDefaultAsync(m => m.StaffID == id);
+
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return View(staff);
+        }
+
+        // POST: Staff/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var staff = await _context.Staff.FindAsync(id);
+            _context.Staff.Remove(staff);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StaffExists(int id)
+        {
+            return _context.Staff.Any(e => e.StaffID == id);
+        }
+    }
+}
