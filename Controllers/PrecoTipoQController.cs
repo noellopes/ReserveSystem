f866a2f7de775ba42d6fde2d6640207528d5ecde
AnaT1999 <anabeatriztavares2004@gmail.com>
2024-11-22 08:58:01 +0000

    era tirar um using -_-

--- Controllers/PrecoTipoQController.cs
+++ Controllers/PrecoTipoQController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
