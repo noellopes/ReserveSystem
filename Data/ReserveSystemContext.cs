f5d055251783b0fd94cea8bb6a96a8d99032dd47
anilhazar <alianilhazar@gmail.com>
2024-11-24 21:38:29 +0000

    Refactor project that SQLite connection can be provided

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -5,18 +5,31 @@ namespace ReserveSystem.Data
 {
     public class ReserveSystemContext : DbContext
     {
-        public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) {}
+        public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options)
+        {
+        }
         public DbSet<Equipamento> Equipamento { get; set; } = default!;
         public DbSet<ClienteModel> ClienteModel { get; set; } = default!;
-        public DbSet<ReservaModel> ReservaModel { get; set; } = default!; 
-        
+        public DbSet<ReservaModel> ReservaModel { get; set; } = default!;
         public DbSet<Sala> Sala { get; set; } = default!;
-        
         public DbSet<TipoSala> TipoSala { get; set; } = default!;
 
-        public void ClearReservaTable() 
-        { 
-            this.Database.ExecuteSqlRaw("DELETE FROM Reserva"); 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            // Configure all string properties globally for SQLite
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(string))
+                    {
+                        property.SetMaxLength(8000); // Safe limit for SQLite compatibility
+                        property.SetColumnType("TEXT");
+                    }
+                }
+            }
         }
 
     }
