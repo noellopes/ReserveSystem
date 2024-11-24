cbe4843c34b0676918ed9b09aa0e4d4cfff7264a
anilhazar <alianilhazar@gmail.com>
2024-11-24 21:58:33 +0000

    Remove SQLite Configurations

--- Program.cs
+++ Program.cs
@@ -4,23 +4,12 @@ using ReserveSystem.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Check if SQLite should be used (local dev)
-var useSQLite = builder.Configuration.GetValue<bool>("UseSQLite", false);
 
-if (useSQLite)
-{
-    // SQLite for local development
-    builder.Services.AddDbContext<ReserveSystemContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("SQLiteConnection") 
-                          ?? throw new InvalidOperationException("Connection string 'SQLiteConnection' not found.")));
-}
-else
-{
     // SQL Server for team
     builder.Services.AddDbContext<ReserveSystemContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("ReserveSystem") 
                              ?? throw new InvalidOperationException("Connection string 'ReserveSystem' not found.")));
-}
+
 
 // Identity Configuration
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
