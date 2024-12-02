e77894b151ca78f4dd9ee4c95b7da1c533031486
filipecampos9 <fjcampos.fc@gmail.com>
2024-12-02 16:26:50 +0000

    Revert "Add SeedData for Ingredients"
    This reverts commit 02d51e7b32e9b2e052fe3cab42282342a1b2594b.

--- Program.cs
+++ Program.cs
@@ -25,11 +25,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    using (var scope = app.Services.CreateScope())
-    {
-        var db = scope.ServiceProvider.GetRequiredService<ReserveSystemContext>();
-        SeedData.Populate(db);
-    }
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
