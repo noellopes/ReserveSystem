02d51e7b32e9b2e052fe3cab42282342a1b2594b
filipecampos9 <fjcampos.fc@gmail.com>
2024-12-02 15:25:08 +0000

    Add SeedData for Ingredients

---
+++ Data/SeedData.cs
@@ -0,0 +1,73 @@
+﻿
+using ReserveSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReserveSystem.Data
+{
+    public class SeedData
+    {
+        internal static void Populate(ReserveSystemContext? db)
+        {
+            if (db == null) throw new ArgumentNullException(nameof(db));
+
+            PopulateIngredients(db);
+        }
+
+        private static void PopulateIngredients(ReserveSystemContext db)
+        {
+            db.Ingredient.AddRange(new List<Ingredient>{
+                new Ingredient { Name = "Ovos", UnityMeasure = "Unidade" },
+                new Ingredient { Name = "Arroz", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Batata", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Frango", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Carne de Porco", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Tomate", UnityMeasure = "Unidade" },
+                new Ingredient { Name = "Cenoura", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Cebola", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Alho", UnityMeasure = "Dente" },
+                new Ingredient { Name = "Pão", UnityMeasure = "Unidade" },
+                new Ingredient { Name = "Leite", UnityMeasure = "Litro" },
+                new Ingredient { Name = "Queijo", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Manteiga", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Azeite", UnityMeasure = "Litro" },
+                new Ingredient { Name = "Óleo", UnityMeasure = "Litro" },
+                new Ingredient { Name = "Farinha", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Açúcar", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Sal", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Pimenta", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Maçã", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Banana", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Laranja", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Melancia", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Uva", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Morango", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Limão", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Coentro", UnityMeasure = "Molho" },
+                new Ingredient { Name = "Salsa", UnityMeasure = "Molho" },
+                new Ingredient { Name = "Hortelã", UnityMeasure = "Molho" },
+                new Ingredient { Name = "Alecrim", UnityMeasure = "Molho" },
+                new Ingredient { Name = "Manjericão", UnityMeasure = "Molho" },
+                new Ingredient { Name = "Louro", UnityMeasure = "Folha" },
+                new Ingredient { Name = "Pimenta do Reino", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Canela", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Cravo", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Noz-moscada", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Gengibre", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Mel", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Vinagre", UnityMeasure = "Litro" },
+                new Ingredient { Name = "Iogurte", UnityMeasure = "Unidade" },
+                new Ingredient { Name = "Chocolate", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Cacau", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Creme de Leite", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Leite Condensado", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Feijão", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Lentilha", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Ervilha", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Grão de Bico", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Abóbora", UnityMeasure = "Kg" },
+                new Ingredient { Name = "Beterraba", UnityMeasure = "Kg" },
+            });
+            db.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -25,13 +25,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<ReserveSystemContext>();
+        SeedData.Populate(db);
+    }
 }
 
 app.UseHttpsRedirection();
