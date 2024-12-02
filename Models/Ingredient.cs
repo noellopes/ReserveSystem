88418ae32c560afe4b4292ce0496fdcac85fbcce
filipecampos9 <fjcampos.fc@gmail.com>
2024-12-02 14:48:55 +0000

    Change some words to PT

--- Models/Ingredient.cs
+++ Models/Ingredient.cs
@@ -7,12 +7,12 @@ namespace ReserveSystem.Models
         
         public int IngredientID { get; set; }
 
-        [Display(Name = "Ingredient Name")]
-        [Required(ErrorMessage = "Ingredient Name is required"), StringLength(300)]
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Nome do ingrediente é obrigatório"), StringLength(300)]
         public string Name { get; set; }
 
-        [Display(Name = "Unity Of Measure")]
-        [Required(ErrorMessage = "Unity of measure is required"), StringLength(10)]
+        [Display(Name = "Unidade de Medida")]
+        [Required(ErrorMessage = "Unidade de medida é obrigatória"), StringLength(10)]
         public string UnityMeasure { get; set; }
 
     }
