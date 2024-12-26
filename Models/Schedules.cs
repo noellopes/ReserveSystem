c3d4f5fd48f4a83da79737d1305ad70cd80c93c9
Rui Ramos <1704711@sal.ipg.pt>
2024-12-26 16:37:28 +0000

    Schedules and TypeOfSchedule

--- Models/Schedules.cs
+++ Models/Schedules.cs
@@ -1,11 +1,23 @@
-﻿namespace ReserveSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
 {
     public class Schedules
     {
         public int SchedulesId { get; set; }
+
+        [Required(ErrorMessage = "StartShiftTime is required.")]
+        [DataType(DataType.DateTime, ErrorMessage = "StartShiftTime not valid.")]
         public DateTime StartShiftTime { get; set; }
+
+        [Required(ErrorMessage = "EndShiftTime is required.")]
+        [DataType(DataType.DateTime, ErrorMessage = "EndShiftTime not valid.")]
         public DateTime EndShiftTime { get; set; }
+
+        [Required(ErrorMessage = "isPrecense is required.")]
         public bool isPrecense { get; set; }
+
+        [Required(ErrorMessage = "isAvailable is required.")]
         public bool isAvailable { get; set; }
 
         public int StaffId { get; set; }
--- Models/TypeOfSchedule.cs
+++ Models/TypeOfSchedule.cs
@@ -1,10 +1,19 @@
-﻿namespace ReserveSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
 {
     public class TypeOfSchedule
     {
         public int TypeOfScheduleId { get; set; }
+
+        [Required(ErrorMessage = "TypeOfScheduleName is required.")]
+        [StringLength(100, ErrorMessage = "TypeOfScheduleName must be 100 chars max.")]
         public string TypeOfScheduleName { get; set; }
+
+        [Required(ErrorMessage = "JobDescription is required.")]
+        [StringLength(500, ErrorMessage = "JobDescription must be 500 chars max.")]
         public string JobDescription { get; set; }
+
         public ICollection<Schedules> schedules { get; set; }
     }
 }
