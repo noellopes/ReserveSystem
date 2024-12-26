7dce2acfb6431b992f46bde5fc3b960fde15d3f2
Rui Ramos <1704711@sal.ipg.pt>
2024-12-26 17:15:21 +0000

    Merge conflict resolve

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -6,6 +6,13 @@ namespace ReserveSystem.Data
     public class ReserveSystemContext : DbContext
     {
         public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) { }
-
+        public DbSet<ReserveSystem.Models.Job> Job { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Staff> Staff { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Schedules> Schedules { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.TypeOfSchedule> TypeOfSchedule { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Job> Job { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Staff> Staff { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Schedules> Schedules { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.TypeOfSchedule> TypeOfSchedule { get; set; } = default!;
     }
 }
