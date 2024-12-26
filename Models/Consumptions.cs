70ff7c4a7d5e82cb54ba54aa8de26268a9c47a60
João Costa <1703762@sal.ipg.pt>
2024-12-26 16:30:10 +0000

    Validações dos campos

--- Models/Consumptions.cs
+++ Models/Consumptions.cs
@@ -1,4 +1,6 @@
-﻿namespace ReserveSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
 {
     public class Consumptions
     {
@@ -6,7 +8,13 @@
         public int RoomId { get; set; } 
         public int ItemId { get; set; }
 
+        [Required(ErrorMessage = "QuantityConsumed is required.")]
+        [Range(1, int.MaxValue, ErrorMessage = "QuantityConsumed must be at least 1.")]
         public int QuantityConsumed { get; set; }
+
+        [Required(ErrorMessage = "ConsumedDate is required.")]
+        [DataType(DataType.Date, ErrorMessage = "Invalid format for ConsumedDate.")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ConsumedDate { get; set; }
     }
 }
--- Models/Item_Room.cs
+++ Models/Item_Room.cs
@@ -1,10 +1,15 @@
-﻿namespace ReserveSystem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
 {
     public class Item_Room
     {
         public int ItemRoomId { get; set; }
         public int RoomTypeId { get; set; } 
         public int ItemId { get; set; }
+
+        [Required(ErrorMessage = "Room quantity is required.")]
+        [Range(1, 100, ErrorMessage = "Room quantity must be between 1 and 100.")]
         public int RoomQuantity { get; set; }
     }
 }
