77b4608635b8a8b0bd347510737599a25eeb45bf
Rui Ramos <1704711@sal.ipg.pt>
2024-12-26 17:20:45 +0000

    Conflit resolve

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -7,9 +7,13 @@ namespace ReserveSystem.Data
     {
    
         public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) { }
-
-        
         public DbSet<ReserveSystem.Models.Job> Job { get; set; } = default!;
         public DbSet<ReserveSystem.Models.Staff> Staff { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Schedules> Schedules { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.TypeOfSchedule> TypeOfSchedule { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Booking> Booking { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Room_Booking> Room_Booking { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Room> Room { get; set; } = default!;
+        public DbSet<ReserveSystem.Models.Room_Type> Room_Type { get; set; } = default!;
     }
 }
