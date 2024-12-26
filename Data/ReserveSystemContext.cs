789db7976c16871fa887d6222127dc12c107b327
RafaelBatista8901 <rafaelbatista8901@gmail.com>
2024-12-26 17:04:43 +0000

    Conflit resolve

--- Data/ReserveSystemContext.cs
+++ Data/ReserveSystemContext.cs
@@ -6,12 +6,13 @@ namespace ReserveSystem.Data
     public class ReserveSystemContext : DbContext
     {
         public ReserveSystemContext(DbContextOptions<ReserveSystemContext> options) : base(options) { }
-        public DbSet<ReserveSystem.Models.Job> Job { get; set; } = default!;
-        public DbSet<ReserveSystem.Models.Staff> Staff { get; set; } = default!;
+
+        
         public DbSet<ReserveSystem.Models.Client> Client { get; set; } = default!;
         public DbSet<ReserveSystem.Models.Booking> Booking { get; set; } = default!;
         public DbSet<ReserveSystem.Models.Room_Booking> Room_Booking { get; set; } = default!;
         public DbSet<ReserveSystem.Models.Room> Room { get; set; } = default!;
         public DbSet<ReserveSystem.Models.Room_Type> Room_Type { get; set; } = default!;
+        
     }
 }
