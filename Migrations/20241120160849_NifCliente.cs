a5076c4c063de6d4c6ccc4f5617461f19383a703
janicehortabd <janicebd2023@gmail.com>
2024-12-31 17:06:04 +0000

    migrationAdd

---
+++ Migrations/20241120160849_NifCliente.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReserveSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class NifCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "NIF",
+                table: "ClienteModel",
+                type: "int",
+                maxLength: 9,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "NIF",
+                table: "ClienteModel",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 9);
+        }
+    }
+}
