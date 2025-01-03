5c293c7b674562539ee5d2c4c2cfba8d5ef0c1e9
Edson Muandula <emuandula69@gmail.com>
2025-01-03 01:26:32 +0000

    Add TipoEquipamento CRUD functionality and views

---
+++ Models/TipoEquipamento.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class TipoEquipamento
+    {
+        [Key]
+        public long IdTipoEquipamento { get; set; }
+        [Required]
+        public string? NomeTipoEquipamento { get; set;}
+
+        public ICollection<Equipamento>? Equipamento{ get; set; }
+    }
+}
