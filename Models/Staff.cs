ba015d874794e4593f7d39c87ee724a5f2786211
Rodrigo Martins <rodasapm@hotmail.com>
2025-01-06 20:39:07 +0000

    Adicionado Tabela de testes

---
+++ Models/Staff.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReserveSystem.Models
+{
+    public class Staff
+    {
+        [Key]
+
+        public int StaffId { get; set; }
+        [Required, Display(Name = "Staff Name")]
+
+        public string Staff_Name { get; set; }
+
+        public string Job_Name { get; set; }
+
+        public virtual ICollection<ExcursaoModel>? Excursao { get; set; }
+    }
+}
