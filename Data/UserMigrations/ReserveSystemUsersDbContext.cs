8c888675e97d3afca3531fe426a7b9a53b09f389
Rui Ramos <1704711@sal.ipg.pt>
2025-01-06 15:52:56 +0000

    Configure ASP.NET Identity to allow roles

--- Data/UserMigrations/ReserveSystemUsersDbContext.cs
+++ Data/UserMigrations/ReserveSystemUsersDbContext.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ReserveSystem.Data.UserMigrations
 {
-    public class ReserveSystemUsersDbContext : IdentityDbContext
+    public class ReserveSystemUsersDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
         public ReserveSystemUsersDbContext(DbContextOptions<ReserveSystemUsersDbContext> options)
             : base(options)
--- Program.cs
+++ Program.cs
@@ -7,49 +7,53 @@ using System.Runtime.InteropServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 {
-    var connectionString = builder.Configuration.GetConnectionString("ReserveSystemsUsers") ?? throw new InvalidOperationException("Connection string 'ReserveSystemsUsers' not found.");
+    var connectionString = builder.Configuration.GetConnectionString("ReserveSystemsUsers")
+        ?? throw new InvalidOperationException("Connection string 'ReserveSystemsUsers' not found.");
     builder.Services.AddDbContext<ReserveSystemUsersDbContext>(options =>
         options.UseSqlServer(connectionString));
 
     builder.Services.AddDbContext<ReserveSystemContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("ReserveSystem") ?? throw new InvalidOperationException("Connection string 'ReserveSystem' not found.")));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("ReserveSystem")
+            ?? throw new InvalidOperationException("Connection string 'ReserveSystem' not found.")));
 }
 else
 {
-    var connectionString = builder.Configuration.GetConnectionString("ReserveSystemsUsersSqlite") ?? throw new InvalidOperationException("Connection string 'ReserveSystemsUsersSqlite' not found.");
+    var connectionString = builder.Configuration.GetConnectionString("ReserveSystemsUsersSqlite")
+        ?? throw new InvalidOperationException("Connection string 'ReserveSystemsUsersSqlite' not found.");
     builder.Services.AddDbContext<ReserveSystemUsersDbContext>(options =>
         options.UseSqlite(connectionString));
 
     builder.Services.AddDbContext<ReserveSystemContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("ReserveSystemSqlite") ?? throw new InvalidOperationException("Connection string 'ReserveSystemSqlite' not found.")));
+        options.UseSqlite(builder.Configuration.GetConnectionString("ReserveSystemSqlite")
+            ?? throw new InvalidOperationException("Connection string 'ReserveSystemSqlite' not found.")));
 }
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddIdentity<IdentityUser, IdentityRole>(
-    options => {
-        // sign in
-        options.SignIn.RequireConfirmedAccount = false;
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    // Sign in options
+    options.SignIn.RequireConfirmedAccount = false;
 
-        // password
-        options.Password.RequireDigit = true;
-        options.Password.RequireLowercase = true;
-        options.Password.RequiredLength = 8;
-        options.Password.RequiredUniqueChars = 6;
-        options.Password.RequireNonAlphanumeric = true;
-        options.Password.RequireUppercase = true;
+    // Password options
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequiredUniqueChars = 6;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
 
-        // Lockout
-        options.Lockout.AllowedForNewUsers = true;
-        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
-        options.Lockout.MaxFailedAccessAttempts = 5;
-    }
-)
+    // Lockout options
+    options.Lockout.AllowedForNewUsers = true;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+})
 .AddEntityFrameworkStores<ReserveSystemUsersDbContext>()
-//.AddDefaultTokenProviders()
+.AddRoleManager<RoleManager<IdentityRole>>() // Add RoleManager support
+.AddDefaultTokenProviders()
 .AddDefaultUI();
 
 builder.Services.AddControllersWithViews();
@@ -66,23 +70,22 @@ if (isDevelopment)
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 using (var servicesScope = app.Services.CreateScope())
 {
-    // Seed Roles
+    // Seed roles
     var roleManager = servicesScope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
     SeedData.PopulateRoles(roleManager);
 
-    // Seed Admin
+    // Seed admin user
     var userManager = servicesScope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
     SeedData.PopulateDefaultAdmin(userManager);
 
     if (isDevelopment)
     {
-        // Seed users
+        // Seed additional users
         SeedData.PopulateUsers(userManager);
 
         // Seed the database
@@ -91,20 +94,13 @@ using (var servicesScope = app.Services.CreateScope())
     }
 }
 
-//seed data
-using (var serviceScope = app.Services.CreateScope())
-{
-    var db = serviceScope.ServiceProvider.GetService<ReserveSystemContext>();
-    SeedData.Populate(db);
-}
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // Ensure this is included
 app.UseAuthorization();
-
-app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
@@ -113,5 +109,3 @@ app.MapControllerRoute(
 app.MapRazorPages();
 
 app.Run();
-
-
