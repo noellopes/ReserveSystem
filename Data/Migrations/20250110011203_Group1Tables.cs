a655eb9ff9a0dff5106399f7215aeceac0577b60
LIBM20 <libm20@outlook.com>
2025-01-10 01:15:22 +0000

    The migration works and created the table.

---
+++ Data/Migrations/20250110011203_Group1Tables.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReserveSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Group1Tables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Booking",
+                columns: table => new
+                {
+                    ID_Booking = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID_Client = table.Column<int>(type: "int", nullable: false),
+                    CheckinDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CheckoutDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BookingDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BookingCode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TotalPersonsNumber = table.Column<int>(type: "int", nullable: false),
+                    PaymentStatus = table.Column<bool>(type: "bit", nullable: false),
+                    Breakfast = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Booking", x => x.ID_Booking);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Room",
+                columns: table => new
+                {
+                    ID_Room = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomTypeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Room", x => x.ID_Room);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomBooking",
+                columns: table => new
+                {
+                    ID_RoomBooking = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ID_Booking = table.Column<int>(type: "int", nullable: false),
+                    ID_Room = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomBooking", x => x.ID_RoomBooking);
+                    table.ForeignKey(
+                        name: "FK_RoomBooking_Booking_ID_Booking",
+                        column: x => x.ID_Booking,
+                        principalTable: "Booking",
+                        principalColumn: "ID_Booking",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomBooking_Room_ID_Room",
+                        column: x => x.ID_Room,
+                        principalTable: "Room",
+                        principalColumn: "ID_Room",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropTable(
+                name: "RoomBooking");
+
+            migrationBuilder.DropTable(
+                name: "Booking");
+
+            migrationBuilder.DropTable(
+                name: "Room");
+
+        }
+    }
+}
