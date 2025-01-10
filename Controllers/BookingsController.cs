3937421c1a9210f999e200b78ee142cf172667a7
rafa <rlima.marques@outlook.pt>
2025-01-10 05:42:26 +0000

    Paging and Search - ish

--- Controllers/BookingsController.cs
+++ Controllers/BookingsController.cs
@@ -54,7 +54,6 @@ namespace ReserveSystem.Controllers
                 }
             }
 
-            
             var model = new BookingViewModel();
 
             model.PagingInfo = new PagingInfo
@@ -76,7 +75,6 @@ namespace ReserveSystem.Controllers
         }
 
 
-
         // GET: Bookings/Details/5
         public async Task<IActionResult> Details(int? id)
         {
@@ -93,112 +91,6 @@ namespace ReserveSystem.Controllers
             }
 
             return View(booking);
-        }
-
-        // GET: Bookings/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Bookings/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID_Booking,ID_Client,CheckinDate,CheckoutDate,BookingDate,BookingCode,TotalPersonsNumber,PaymentStatus,Breakfast")] Booking booking)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(booking);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(booking);
-        }
-
-        // GET: Bookings/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Booking.FindAsync(id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-            return View(booking);
-        }
-
-        // POST: Bookings/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID_Booking,ID_Client,CheckinDate,CheckoutDate,BookingDate,BookingCode,TotalPersonsNumber,PaymentStatus,Breakfast")] Booking booking)
-        {
-            if (id != booking.ID_Booking)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(booking);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!BookingExists(booking.ID_Booking))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(booking);
-        }
-
-        // GET: Bookings/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var booking = await _context.Booking
-                .FirstOrDefaultAsync(m => m.ID_Booking == id);
-            if (booking == null)
-            {
-                return NotFound();
-            }
-
-            return View(booking);
-        }
-
-        // POST: Bookings/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var booking = await _context.Booking.FindAsync(id);
-            if (booking != null)
-            {
-                _context.Booking.Remove(booking);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool BookingExists(int id)
