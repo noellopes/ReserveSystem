5cedcf4e73846c4abc794398ba6e71f661d66a4e
Rodrigo Paula <rnpaula2000@gmail.com>
2025-01-10 00:50:27 +0000

    final

---
+++ Models/JobIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ReserveSystem.Models
+{
+    public class JobIndexViewModel
+    {
+        public List<JobModel> Jobs { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        public bool HasPreviousPage => CurrentPage > 1;
+        public bool HasNextPage => CurrentPage < TotalPages;
+    }
+}
